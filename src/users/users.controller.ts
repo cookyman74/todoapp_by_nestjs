282a57329539ba404517779fa72761176a464416
Justin Jang <cookyman@gmail.com>
2024-09-22 13:37:29 +0900

    리팩토링 - 오타수정 - 불필요 패키지 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dto/create-user.dto";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,8 +8,8 @@ export class UsersController {
 
   // 새로운 사용자 등록
   @Post()
-  createUser(@Body() creeateUserDto: CreateUserDto) {
-    return this.usersService.create(creeateUserDto);
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Get(':id')
